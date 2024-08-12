cb0ae162ab77121916b38d058e32fbf0b1ba2aab
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-08-12 20:14:29 +0530

    Updating grading logic

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import linkedin_graper
 import scrapper
+import re
 from flask import Flask, request, jsonify
 import pandas as pd
 import os
-
 
 session_cookie = os.getenv("SESSION_COOKIE")
 api_key = os.getenv("OPENAI_API_KEY")
@@ -11,7 +11,6 @@ api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 
 @app.route('/scrape_and_grade', methods=['POST'])
-
 def scrape_and_grade():
     data = request.json
     profile_name = data.get('profile_name')
@@ -29,16 +28,37 @@ def scrape_and_grade():
     grading_parameters = pd.read_excel('grading_parameters.xlsx')
 
     # Grade the LinkedIn profile
-    warm, deep, wide = linkedin_graper.grade_linkedin_profile(grading_parameters, profile_data,api_key)
+    warm, deep, wide = linkedin_graper.grade_linkedin_profile(grading_parameters, profile_data, api_key)
+
+    # Extract the scores using regex
+    # warm_match=warm.split("/n")[-1].strip()
+    # if warm_match:
+    #     warm_score=warm_match.group(1)
+    # else:
+    #     warm_score=None
+
+    # deep_match=deep.split("/n")[-1].strip()
+    # if deep_match:
+    #     deep_score=deep_match.group(1)
+    # else:
+    #     deep_score=None
+
+    # wide_match=wide.split("/n")[-1].strip()
+    # if wide_match:
+    #     wide_score=wide_match.group(1)
+    # else:
+    #     wide_score=None
 
     # Return the results as a JSON response
     return jsonify({
         'profile_data': profile_data,
         'warm': warm,
         'deep': deep,
-        'wide': wide
+        'wide': wide,
+        "warm_score": warm.split("/n")[-1][-4:-1].strip(),
+        "deep_score": deep.split("/n")[-1][-4:-1].strip(),
+        "wide_score": wide.split("/n")[-1][-4:-1].strip()
     })
-
 
 if __name__ == '__main__':
     app.run(port=5000)
